833af83f36afd87cd8edc8daaf59659d5f31d1da
Gias Kay Lee <balancetraveller+github@gmail.com>
2013-09-01 17:23:12 +0800

    Step 1 (Amendment): Change residual references to the old Bower installation directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -247,7 +247,7 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             '.htaccess',
-            'bower_components/**/*',
+            'components/**/*',
             'images/{,*/}*.{gif,webp}',
             'styles/fonts/*'
           ]
--- karma.conf.js
+++ karma.conf.js
@@ -11,12 +11,12 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-resource/angular-resource.js',
-      'app/bower_components/angular-animate/angular-animate.js',
-      'app/bower_components/angular-sanitize/angular-sanitize.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/components/angular/angular.js',
+      'app/components/angular-route/angular-route.js',
+      'app/components/angular-resource/angular-resource.js',
+      'app/components/angular-animate/angular-animate.js',
+      'app/components/angular-sanitize/angular-sanitize.js',
+      'app/components/angular-mocks/angular-mocks.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
