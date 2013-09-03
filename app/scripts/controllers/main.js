62183881b8fe79ed5ac3facd2e488f4ec598fcc0
Gias Kay Lee <balancetraveller+github@gmail.com>
2013-09-03 18:09:01 +0800

    Step 4: Filter data using user input with default value

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,4 +6,10 @@ controller('MainCtrl', function(
     $resource
 ){
     this.resource = $resource('/data/taiwan-percentage-of-young-population.json').query();
+
+    this.resource.$promise.then(function(data) {
+        this.filter = {
+            year: data[data.length - 1].year
+        };
+    }.bind(this));
 });
